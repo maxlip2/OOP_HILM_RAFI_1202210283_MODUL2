5023e6e3fcc933330f23d807d1365628f0105059
Rafi Abdul Fatah <64337310+maxlip2@users.noreply.github.com>
2022-12-05 09:04:08 +0700

    Add files via upload

---
+++ OOP_HILM_RAFI_1202210283_MODUL2/src/TransportasiAir.java
@@ -0,0 +1,26 @@
+public class TransportasiAir {
+
+    //attribute
+    protected int JumlahKursi;
+    protected int Biaya;
+
+    //constructor
+    public void informasi(int JumlahKursi, int Biaya){
+        this.JumlahKursi = JumlahKursi;
+        this.Biaya = Biaya;
+    }
+
+    //method public
+    public void informasi(){
+        System.out.println();
+    }
+
+    public void berlayar(){
+        System.out.println();
+    }
+    
+    public void berlabuh(){
+        System.out.println();
+    }
+    
+}
